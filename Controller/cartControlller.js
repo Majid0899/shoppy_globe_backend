34a52c3e3474fea08ca2dc61347ae6f527d6fb9e
Majid0899 <majidkhan0899@gmail.com>
2025-08-08 10:17:51 +0530

    CRUD functionality  for cart is completed

--- Controller/cartControlller.js
+++ Controller/cartControlller.js
@@ -22,7 +22,7 @@ async function handleAddToCart(req, res) {
       return res.status(400).json({ error: "Not enough Stock are availabe" });
     }
 
-    /*Initialize a cart if it is Empty */
+    /*Initialize a cart if it is Empty  */
     let cart = await Cart.findOne();
     if (!cart) {
       cart = new Cart({ items: [] });
@@ -36,23 +36,119 @@ async function handleAddToCart(req, res) {
     const existingItem = cart.items.find(
       (item) => item.product.toString() === product_id
     );
-    
+
     if (existingItem) {
       existingItem.quantity += quantity;
     } else {
       cart.items.push({ product: product_id, quantity });
     }
-    
 
     //save the cart item in database;
-     await cart.save();
+    await cart.save();
 
-    return res.status(200).json({ message: 'Product added to cart', cart });
+    return res.status(200).json({ message: "Product added to cart", cart });
   } catch (error) {
-
-   
     res.status(500).json({ error: "Internal Server Error" });
   }
 }
 
-export { handleAddToCart };
+async function handleUpdateQuantity(req, res) {
+  try {
+    //extract the product_id
+    const product_id = req.params.id;
+
+    //Extract the product_id and quantity
+    const { quantity } = req.body;
+
+    //Check for the valid product_id and quantity
+    if (!product_id || !quantity || quantity < 1) {
+      return res.status(400).json({ error: "Invalid Product ID or Quantiy" });
+    }
+
+    /*Retreive the CartList
+     *Assign to a cart
+     */
+    const cartList = await Cart.find();
+    const cart = cartList[0];
+
+    // Check for the product id is correct or not
+    const existingProduct = cart.items.find(
+      (item) => item.product.toString() === product_id
+    );
+    if (!existingProduct) {
+      return res.status(404).json({ error: "Product is not found" });
+    }
+
+    /**
+     * Extract the product using product id present in database;
+     * Need stock value to check the quantity is not greater than stock
+     */
+    const existingProductData = await Products.findById(
+      existingProduct.product.toString()
+    );
+
+    if (existingProductData.stock < quantity) {
+      return res.status(400).json({ error: "Not enough Stock are availabe" });
+    }
+
+    //Updat the quantity
+    existingProduct.quantity = quantity;
+
+    //save in the database
+    await cart.save();
+
+    res.status(200).json({ cart, message: "Updated successfully" });
+  } catch (error) {
+    
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+}
+
+async function handleRemoveProduct(req, res) {
+  try {
+
+    //extract the item id
+    const item_id = req.params.id;
+
+    /*Retreive the CartList
+     *Assign to a cart
+     *extract the items  and assign it to variable
+     */
+    const cartList = await Cart.find();
+    const cart = cartList[0];
+    const items = cart.items;
+   
+
+    //Check for the item is exist in cart or not
+    const isItem = items.findIndex((item) => item.id === item_id);
+    if (isItem === -1) {
+      return res.status(400).json({ error: "product not in cart" });
+    }
+
+    /**
+     * Update the items in cart
+     * Save in database
+     */
+    items.splice(isItem, 1);
+    await cart.save();
+
+    
+    res.status(200).json({ message: "Deleted Successfully" });
+  } catch (error) {
+    
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+}
+
+async function handleCartItems(req,res) {
+  try {
+    const cartList=await Cart.find();
+    res.status(200).json({carts:cartList[0].items})
+    
+  } catch (error) {
+       res.status(500).json({ error: "Internal Server Error" });
+  }
+  
+}
+
+export { handleAddToCart, handleUpdateQuantity, handleRemoveProduct,handleCartItems };
--- Routes/cartRoutes.js
+++ Routes/cartRoutes.js
@@ -1,8 +1,12 @@
 import express from "express";
-import { handleAddToCart } from "../Controller/cartControlller.js";
+import { handleAddToCart, handleRemoveProduct, handleUpdateQuantity,handleCartItems } from "../Controller/cartControlller.js";
 const router=express.Router();
 
 router.post("/add",handleAddToCart)
+router.put("/:id",handleUpdateQuantity)
+router.delete("/:id",handleRemoveProduct)
+router.get("/",handleCartItems)
+
 
 
 export default router;
